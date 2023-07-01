a8bfc742dde8a17329c6267bbb1127180714d843
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-07-01 09:00:30 +0100

    Add files via upload

---
+++ Beginning_programming_CforD/ex0201.c
@@ -0,0 +1,11 @@
+//First Example
+//Exercise 02.01
+
+#include<stdio.h>
+
+int main()
+{
+puts("Hello Human.");
+
+
+}
---
+++ Beginning_programming_CforD/ex0301.c
@@ -0,0 +1,9 @@
+//First Example
+//Exercise 03.01
+
+#include<stdio.h>
+
+int main()
+{
+printf("4 plus 5 is %d\n" , 4+5);
+}
