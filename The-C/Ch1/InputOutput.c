14718849cbbb652fccb760e0df7b24fa5c98638b
playdbyez <131136399+playdbyez@users.noreply.github.com>
2023-07-03 16:11:32 +0100

    Add files via upload

---
+++ The-C/Ch1/InputOutput.c
@@ -0,0 +1,25 @@
+//basic Input / Output
+#include <stdio.h>
+
+int main ()
+{
+char x;
+x = getchar();
+
+printf("%c", x);
+
+//Only returns 1 character from input thanks to %c
+
+
+
+
+char* y = "She grind on me she 	skating!";
+printf("%s", y);
+
+//Outputs whole string thanks to %s
+
+
+
+//how to implement getchar()?
+//how to implement scanf()?
+}
