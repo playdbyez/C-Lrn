c20087f9a7c9a19d4e16b5dcca26a5fa0565f235
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-03-21 12:05:15 +0000

    Create pointerAlteration1.c

---
+++ IntermCProg/Pointers/pointerAlteration1.c
@@ -0,0 +1,29 @@
+//Re-assign the value of q[10] by using an external pointer
+// Possible since ptr can point to a dereference ampersand
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+int *ptr;
+
+int q[15];
+    q[10]= 7;
+
+     //ptr++;
+    // uncommenting the line above results in the Output : 7
+    
+    // &ptr = &q[10]
+    // assignment is invalid
+    
+    ptr =  &q[10];
+    *ptr = 9;
+    printf("\n%d", q[10]);
+    
+      //printf("\n%d", q[11]);
+      // uncommenting the ptr++ plus this line will Output: 9  
+}
+// Output: 9
