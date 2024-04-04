c7fed450c3b31625682727e7c54b0e9638346202
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-04-04 18:06:26 +0100

    Create Security_mod.c

---
+++ IntermCProg/Standard/Toolsz/Security_mod.c
@@ -0,0 +1,32 @@
+//11.2
+// Using a wrong index to modify a program's behaviour
+// the line with the hex value may have to be altered depending on the output of the following commands
+//$ gcc -version 
+//$ more /proc/cpuinfo
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void f0() {}
+void f1 (void) 
+{
+  char name[10] = {9, 8 ,7, 6, 5, 4, 3, 2, 1, 27};
+  name [24] = 0x73;
+  f0();
+}
+
+void f2 (void)
+{printf ("f2 has done something bad\n");}
+
+
+int main (int argc, char * argv[])
+{
+f1();
+  return EXIT_SUCCESS;
+}
+
+
+// Output:
+          f2 has done something bad
+          Segmentation fault (core dumped)
