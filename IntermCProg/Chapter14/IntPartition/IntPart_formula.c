cffbdfc5b9bc0880436b80edbf2151e9753badaf
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-04-08 10:32:03 +0100

    Create IntPart_formula.c

---
+++ IntermCProg/Chapter14/IntPartition/IntPart_formula.c
@@ -0,0 +1,24 @@
+//How many ways can the positive integer n be partitioned into smaller values ?
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int f(n){
+		int i;
+		int sum =0;
+		if (n ==1) {return 1;}
+	for (i = ; i <n; i++){ sum += f(n-i);}
+		sum++;
+			return sum;				}
+
+int main (int argc, char *argv[])
+{ 
+	if (argc < 2) {printf("ERROR: Need a positive integer \n"); return -1;}
+			n = (int) strtol(argv[1], NULL, 10);
+	if (n <= 0) {printf("ERROR: Need a positive integer \n"); return -1;}
+
+printf("f(%d) = %d\n", n, f(n));
+ 
+return 0;
+
+}
