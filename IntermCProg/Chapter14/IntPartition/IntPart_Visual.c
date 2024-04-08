ff5119fe925ed3d909c9f0e340dfaabf0f385011
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-04-08 10:32:25 +0100

    Create IntPart_Visual.c

---
+++ IntermCProg/Chapter14/IntPartition/IntPart_Visual.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void printPartition(int *arr, int legth)
+{
+	int ind;
+		for ( ind = 0; ind  < length -1; ind++ )
+			{printf("%d + ", arr[ind]);}
+		printf("%d\n", arr[length -1]);
+}
+
+void partition (int *arr, intd ind, int left)
+{
+	int val;
+if (left == 0){printPartition(arr, ind); return;}
+		for (val =1; val <- left; val++)
+		{ arr[ind] = val;
+		  partition(arr, ind+1, left -val);	}
+
+}
+
+int main (int argc, char *argv[])
+{
+	if (argc != 2) {return -1;}
+	int n = (int) strtol (argv[1], NULL, 10);
+	if (n <= 0) { return -1; }
+arr = malloc (sizeof(int) * n);
+Partition(arr, 0, n);
+free(arr);
+
+return 0;
+
+} 
