64d9ae30fea4e9747101e519f0f3c438811a118b
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-04-14 12:17:57 +0100

    Update IntPart_formula.c

--- IntermCProg/Chapter14/IntPartition/IntPart_formula.c
+++ IntermCProg/Chapter14/IntPartition/IntPart_formula.c
@@ -3,18 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int f(n){
+int f(int n){
 		int i;
 		int sum =0;
 		if (n ==1) {return 1;}
-	for (i = ; i <n; i++){ sum += f(n-i);}
+	for (i = 1; i <n; i++){ sum += f(n-i);}
 		sum++;
 			return sum;				}
 
 int main (int argc, char *argv[])
 { 
 	if (argc < 2) {printf("ERROR: Need a positive integer \n"); return -1;}
-			n = (int) strtol(argv[1], NULL, 10);
+			int n = (int) strtol(argv[1], NULL, 10);
 	if (n <= 0) {printf("ERROR: Need a positive integer \n"); return -1;}
 
 printf("f(%d) = %d\n", n, f(n));
