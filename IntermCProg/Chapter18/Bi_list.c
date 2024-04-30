b17b78717890781c4c1d749d838351405ba70086
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-04-30 11:01:19 +0100

    Create Bi_list.c

---
+++ IntermCProg/Chapter18/Bi_list.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct listnode
+{
+  struct listnode *next;
+  struct listnode *prev;
+  int value;
+  int index;
+}Node;
+
+static Node *Node_construct(int val)
+{
+  Node *nd = malloc (sizeof (Node));
+  nd -> value = val;
+  nd -> next = NULL;
+  nd -> prev = NULL;
+  return nd;
+}
+
+Node *List_Binsert(Node *head, int val)
+{
+  //printf("Insert at TAIL (%d)\n", val);
+  Node *ptr = Node_construct(val);
+  if (head == NULL)
+  {   ptr -> next = head;
+      ptr -> prev = head; 
+      ptr-> value = val;  }
+    
+
+  else { 
+    Node *qtr = ptr->next;
+      ptr -> next = head;
+      ptr -> prev = NULL;  
+      ptr -> value = val;
+    
+  }
+return ptr;                    
+}
+
+  int main ()
+{
+Node *head;
+//head -> index = 0;
+  head = List_Binsert(head,878);
+  head = List_Binsert(head,5);
+  head = List_Binsert(head,511);
+  
+  while (head != NULL)
+  {
+      printf ("%d\n",head-> value);
+      head = head -> next;
+  }
+  
+  
+    return 0;
+}
+  
