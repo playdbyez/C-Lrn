53518efd23db5e37f837d488e749f9a7faea4a69
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-06-01 15:41:13 +0100

    Create PossiblShuffls.c

---
+++ IntermCProg/Chapter14/PossiblShuffls.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main()
+{
+    char deck[] = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K'};
+char *Rdek;
+char *Ldek;
+
+
+
+//Output
+for (int i =0; i < strlen(deck); i++){ printf("%c ", deck[i]); }
+   
+   
+    return 0;
+}
