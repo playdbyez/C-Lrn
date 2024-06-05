bf8bcdc0cd113d986a54717b66b7c56adf43b9d6
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-06-05 12:25:34 +0100

    Update and rename RiffleShuffle.c to RiffleCombos.c

---
+++ IntermCProg/Chapter14/Riffle/RiffleCombos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int main()
+{
+    char deck[][2] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+char *Rdek;
+char *Ldek;
+
+time_t t;
+srand ((unsigned) time(&t));
+   
+    return 0;
+}
