5f3fb2c9bc43bc0c90b94d957c1548fac73990d3
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-06-15 11:53:15 +0100

    Update Stack_sort.c

--- Personal/Sorting/Stack_sort.c
+++ Personal/Sorting/Stack_sort.c
@@ -22,13 +22,15 @@
 // 8- Once all values have been iterated through, the contents of the stack are popped to the output from last inserted until the end of the linked list
 
 
+//Program checks if the list is stack sortable or not 
+#include <stdio.h>
 
 int findIndex(int *arr, int first, int last, int maxmin)
 {
   int ind;
   int answer = first;
   for(ind = first + 1; ind <= last; ind++)
-    { if(maxmin == 1  &&  arr[answer] < arr[ind]  || maxmin == 0  &&  arr[answer] > arr[ind] ) { answer = ind; } }
+    { if(( (maxmin == 1)  &&  (arr[answer] < arr[ind]) )  || ( (maxmin == 0)  &&  (arr[answer] > arr[ind]) ) ) { answer = ind; } }
 return answer;
 }
 
@@ -48,4 +50,26 @@ int isStackSortable(int *arr, int first, int last)
   int sortA = isStackSortable(arr, first, maxIndex - 1); 
   int sortB = isStackSortable(arr, maxIndex + 1, last);
     return (sortA && sortB);
+}
+
+
+
+
+int main()
+{
+    int setA[] = { 10, 4, 33, 5, 9, 2, 14, 57, 9, 3 };
+    int setB[] = { 1, 5, 2, 3, 5, 4 };
+    int setC[] = { 2, 3, 1 };
+    int setD[] = { 5, 1, 11 };
+    int lenA = sizeof(setA)/ sizeof(setA[0]);
+    int lenB = sizeof(setB)/ sizeof(setB[0]);
+    int lenC = sizeof(setC)/ sizeof(setC[0]);
+    int lenD = sizeof(setD)/ sizeof(setD[0]);
+    //printf("%d", lenD);
+  
+   printf("setA (%d)\n", isStackSortable(setA, 0, lenA));
+   printf("setB (%d)\n", isStackSortable(setB, 0, lenB));
+   printf("setC (%d)\n", isStackSortable(setC, 0, lenC));
+   printf("setD (%d)\n", isStackSortable(setD, 0, lenD));
+    return 0;
 }
