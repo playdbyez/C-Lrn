b4fa96aa5a4fe6f19420e0f9ee34de9f7d453d40
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-10-01 08:32:29 +0100

    Update intGuide.cpp

--- CPP-Lrn/C Primer (5ᵗʰ) 2012/Ch3/intGuide.cpp
+++ CPP-Lrn/C Primer (5ᵗʰ) 2012/Ch3/intGuide.cpp
@@ -1,33 +1,35 @@
 
-
 Int MEMORY sizes (bits)
+// Consult the <climits> library macros for more information
 
 
 //normal:
-			int AKA int long
-	-2,147,483,648     to      +2,147,483,647 
+int AKA int long
+-2,147,483,648     to      +2,147,483,647 
 	
-	
-			  short int
-		-32,768      to      +32,767 
+
+//short int
+-32,768      to      +32,767 
 		
 		
 	
 		
 //unsigned:
+unsigned int  AKA unsigned long int
+0	to        4,294,967,295.
+		
+		
+		
+//unsigned short int
+0        to         65,535
+		
+//unsigned long long [20 digits long]
+0        to         2⁶⁴-1 ≈ 18446744073709551615
 
-		unsigned int  AKA unsigned long int
-		0		to        4,294,967,295.
-		
-		
-		
-			unsigned short int
-		0              to         65,535
-		
-		
+# include <climits>
+unsigned long long var = UULONG_MAX;
+// The macro from <climits> UULONG_MAX == 18446744073709551615	
+// in case of a value overflow its value maps back to 0
+// in case of a value underflow its value maps back to 18446744073709551615
 
 
-//Alternatively use "sizeof" operator to return the number of bytes allocated to the variable
-
-int var;
-sizeof( var );
