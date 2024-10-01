0cb6da38b56ee66e8e2f7a4fcf5df843d0955bc9
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-10-01 09:03:01 +0100

    Create mallOverflow1.c

---
+++ IntermCProg/Chapter7/mallOverflow1.c
@@ -0,0 +1,22 @@
+/increments the size of the ptr until we hit a memory address past the '\0';
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+int n;
+int *ptr;
+int inc = 0;
+
+n = 4;
+ptr = (int*)malloc(n * sizeof (int));
+    ptr[1] = 2;
+    while (inc < n+3){
+    printf ("%d ", ptr[inc]);
+    inc++;
+        
+    }
+    
+}
