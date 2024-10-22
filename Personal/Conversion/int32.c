7bcf9fbf5c53f5b8d418f2ccf31096b29203000b
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-10-22 09:34:15 +0100

    Update int32.c

--- Personal/Conversion/int32.c
+++ Personal/Conversion/int32.c
@@ -1,27 +1,7 @@
-// IP decimal range
+// IP decimal range 2³²
 // each section of the address represents 1 byte
 // output is represented in bits
-// last output 2³²
-#include  <stdio.h>
 
-int main()
-{
-    unsigned  long f = 1;
-    printf ("1 ");
-    for (int i = 1; i <= 32; i++)
-    {
-        if (i % 8 == 0 && i != 0){printf("\n");}
-        if (i <32)printf("%ld ", f*=2);
-        if (i == 32)printf("%ld ", (f*=2) -1);
-    }
- 
-    return 0;
-}
-
-
-
-
-//Implementation v2
 
 #include  <stdio.h>
 #include  <math.h>
