424f50f3fa5dfb169a9dd70f33a5f3f31ff664ac
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-11-20 09:14:56 +0000

    Update int2Str.c

--- Personal/Conversion/int2Str.c
+++ Personal/Conversion/int2Str.c
@@ -61,3 +61,63 @@ x --;
  
     return 0;
 }
+
+
+//v.2
+//pointer issue at cycle i =5
+
+#include  <stdio.h>
+#include  <stdint.h>
+#include  <math.h>
+ 
+int main()
+{
+    uint32_t n = 65891;
+    
+ 
+    float vl = log10(n)+1;
+    int len = vl;
+    char str[len];
+    
+    uint8_t x = len-2;
+    uint8_t y = len-2;
+ 
+ 
+    uint32_t dec1 = 10;
+    uint32_t dec2 = 10;
+    uint8_t j = 0;
+    uint32_t k = 0;
+    uint32_t l = 1;
+    uint32_t g = 0;
+    double   p ;
+ 
+    for (unsigned i = 0; i < 3; i++){
+ 
+        while (j < x){ dec1*= 10;j++;}
+        if(i == 0){g = n/dec1; str[i] = g+48;}
+        j=0;
+       
+        
+        
+        //printf("%ld\n", dec2); printf("%ld.\n", dec1);
+        
+ 
+ 
+        if (i >0){ while (j < y){ dec2*= 10;j++;}
+            l=10;
+            k = (n/dec1); //printf("%d\n",k ); 
+            p = (n/dec2); //printf("%f\n", p);  
+            g = k - (p*l); printf("%ld\n", g);
+            
+                  y--;
+        }
+j =0;
+dec1 = 10;
+dec2 = 10;
+x--;
+        //printf("%lld\n",k); 
+}
+ 
+ 
+    return 0;
+}
