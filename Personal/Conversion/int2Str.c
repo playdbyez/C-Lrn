699f86a9cbf1557a9ae810eb7c0930cf6be611b9
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-11-25 11:54:08 +0000

    Update int2Str.c

--- Personal/Conversion/int2Str.c
+++ Personal/Conversion/int2Str.c
@@ -4,120 +4,41 @@
  
 int main()
 {
-    uint32_t n = 65891;
-    char *str;
- 
-    float vl = log10(n)+1;
-    int len = vl;
-    uint8_t x = len-2;
-    uint8_t y = len-3;
- 
- 
-    uint32_t dec1 = 10;
-    uint32_t dec2 = 10;
-    uint8_t j = 0;
-    uint32_t k = 0;
-    uint32_t l = 1;
-    uint32_t g = 0;
-    float   p ;
- 
-    for (unsigned i = 0; i < 2; i++){
- 
-        while (j < x){ 
-            dec1*= 10; 
- 
-            if (i >0){ dec2*=10;}
-            j++;
-        }
- 
-        if(i == 0){g = n/dec1; str[i] = g-49;}
- 
- 
-        if (i >0){
-            l*=10;
-            p = (n/dec2); printf("%f", p);
-            k = p*l;
-            p = n/dec1;
-            g = p*l;
-            dec2 = 10;
-        }
-j =0;
-dec1 = 10;
-x --;
-        printf("%lld\n",k); 
-}
- 
- 
- 
- 
- 
-//    
- 
- 
-//    if (i > 0) {}
-    //decrement x for every i cycle
-    //x--;
- 
- 
-    return 0;
-}
-
-
-//v.2
-//pointer issue at cycle i =5
-
-#include  <stdio.h>
-#include  <stdint.h>
-#include  <math.h>
- 
-int main()
-{
-    uint32_t n = 65891;
-    
+    uint64_t n = 42949672961158;
+    uint64_t m = n;
  
     float vl = log10(n)+1;
     int len = vl;
     char str[len];
     
-    uint8_t x = len-2;
-    uint8_t y = len-2;
- 
- 
-    uint32_t dec1 = 10;
-    uint32_t dec2 = 10;
+    uint8_t x = len-1;
     uint8_t j = 0;
-    uint32_t k = 0;
-    uint32_t l = 1;
-    uint32_t g = 0;
-    double   p ;
  
-    for (unsigned i = 0; i < 3; i++){
  
-        while (j < x){ dec1*= 10;j++;}
-        if(i == 0){g = n/dec1; str[i] = g+48;}
-        j=0;
+    uint64_t dec = 1;
+    
+    uint64_t k = 0;
+    uint8_t g = 0;
+
+ 
+    for (unsigned i = 0; i < len; i++){
+ 
+        while (j < x){ dec*= 10;j++;}
+        g = m/dec; 
+        str[i] = g+48;
        
         
+        k =  g * dec;
+        m-=k;
         
-        //printf("%ld\n", dec2); printf("%ld.\n", dec1);
-        
- 
- 
-        if (i >0){ while (j < y){ dec2*= 10;j++;}
-            l=10;
-            k = (n/dec1); //printf("%d\n",k ); 
-            p = (n/dec2); //printf("%f\n", p);  
-            g = k - (p*l); printf("%ld\n", g);
             
-                  y--;
-        }
 j =0;
-dec1 = 10;
-dec2 = 10;
+dec = 1;
 x--;
-        //printf("%lld\n",k); 
+        
 }
  
+    printf("%s", str);
  
     return 0;
 }
