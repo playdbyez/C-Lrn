598842a5df63a143b35429b333f5c1cc71992084
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-11-29 19:49:33 +0000

    Update int33.c

--- Personal/Conversion/int33.c
+++ Personal/Conversion/int33.c
@@ -7,8 +7,7 @@ static void printArray (char *arr, uint8_t len);//prints binArray
 char *arrBin(uint16_t size, uint32_t n);//conv to bin array
                                         //evaluates output size and 32bInt
 
-
-static void dPrint (char *arr, uint8_t len);// re-orders binArray and prints to decimal
+static void dPrint (char *arr, uint8_t st,  uint8_t nd);// re-orders binArray and prints to decimal
     
 
 
@@ -18,9 +17,9 @@ int main()
 {
     uint32_t n = 3021966218;
     
-   int *arr = arrBin(36, n);
-   //dPrint(arr, 36);
-   //printArray(arr, 36);
+   int *arr = arrBin(35, n);
+   dPrint(arr, 7, 0);
+   //printArray(arr, 35);
     free(arr);
 }
 
@@ -57,38 +56,37 @@ static void printArray (char *arr, uint8_t len)
     return;
 }
 
-
-static void dPrint (char *arr, uint8_t len)
+static void dPrint (char *arr, uint8_t st,  uint8_t nd )
 {
- uint8_t j = 28;
- uint8_t k = 35;
- uint32_t tt = 0;
+    // 7 - 0
+    // 16 - 9
+    // 25 - 18
+    // 34 - 27
+    
+    
+ int8_t j = st-1;
+ uint8_t k = nd;
     uint64_t inc = 1;
+   
+ uint32_t tt = 0;
+    if (arr[st] == '1') {tt = 1;}
     
-    for (uint8_t i = 0; i < 4; i++){
-        if (arr[27] == '1' && i == 0) {tt++;}
-        if (arr[18] == '1'&& i == 1) {tt++;}
-        if (arr[9] == '1'&& i == 2) {tt++;}
-        if (arr[0] == '1'&& i == 3) {tt++;}
-        if(i == 1){j = 19; k = 26;}
-        if(i == 2){j = 10; k = 17;}
-        if(i == 3){j = 1; k = 8;}
+  
         
-     for ( j = j; j < k; j++){
-     
-         
-         inc*=2;
-         if (arr[j] == '1'){tt+=inc;}
-         
+     for ( ; j >= k; j--){ inc*=2;
+                           if (arr[j] == '1'){tt+=inc;}  }
         
-     }
-        inc = 1;
-        printf ("%d." ,tt);
-        tt = 0;
-        
-        
-    }
- 
+        printf ("%d." ,tt); 
     
-    return;
+    if (st == 7){  return dPrint(arr,16,9); }
+    if (st == 16){  return dPrint(arr,25,18); }
+    if (st == 25){  return dPrint(arr,34,27); }
+        
+        
+    // the last return
+    //if (st >=34) { printf ("%d\n", tt);return;}
+    
+    
+   
 }
+
