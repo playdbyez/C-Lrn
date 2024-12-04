40a68e8d725742d6fcd234efe5ae3f07549bfdb2
playdbyez <131136399+playdbyez@users.noreply.github.com>
2024-12-04 14:03:50 +0000

    Update int32.c

--- Personal/Conversion/int32.c
+++ Personal/Conversion/int32.c
@@ -1,137 +1,48 @@
 // IP decimal range 2³²
 // each section of the address represents 1 byte
-// output is represented in bits
+
 
 
 #include  <stdio.h>
-#include  <math.h>
+#include  <string.h>
+#include  <stdint.h>
+#include  <stdlib.h>
 
-int main()
+
+char *arrBin(uint16_t size, uint32_t n);
+//conv to bin array
+//evaluates output size and 32bInt
+
+static void dPrint (char *arr, uint8_t st,  uint8_t nd);
+// prints binArray to decimal
+// evaluates each 8bit section back-to-front
+
+
+int main() 
 {
-    unsigned long max = 4294967295;
+    uint32_t n = 3021966218;
+    //Input
     
-    unsigned long n = 3021966218;
+    uint32_t max = 4294967295;
     double sz = log10(n)+1;
-    int sizen = sz;
-    
+    uint16_t sizen = sz;
     if (n > max){printf("ERROR: Invalid entry\n");}
     if (n < 1)  {printf("ERROR: Invalid entry\n");}
-    
-    if (n < 16){} //4 
-    if (n < 256){} //8
-    if (n < 4096){} //12
-    if (n < 65536){} //16
-    if (n < 1048576){} //20
-    if (n < 16777216){} //24
-    if (n < 268435456){} //28
-    if (n < 4294967296){} //32
+    //Input handling
     
     
-    
-    unsigned long f = 1;
-    
-    
-    for (int i  = 1; i <= 32; i++)
-    {   
-        f*=2; 
-        if (i%4 == 0) {printf("%ld ", f);}
-    }
-    
-    /*
-    for (int i  = 0; i <= sizen; i++)
-    {
-        printf("%d ", n & (1 << i));
-    }
-    */
-    //printf ("%d",sizen);
-    
-    return 0;
+   int *arr = arrBin(35, n);
+   dPrint(arr, 7, 0);
+   free(arr);
 }
+//Driver code
 
 
 
-///
 
 
 
-#include  <stdio.h>
-#include  <math.h>
-#include  <stdint.h>
-#include  <stdlib.h>
- 
-char * inflow ( char * arr, uint32_t b)
-{ 
-uint8_t j = 0;
-for(int i = 31; i <= 0; i--)
-{ 
-if (j != 0 && (j % 8 )==0 ){
-                  if ( j != 8 ) {   j++; a[j] = '.';}
-                  if ( j == 8 ) {   a[j] = '.'; j++;}  }
-    
-a[j] = ( b &(1<<i));
-j++;
-}
-return *a;
-}
-
-
-static void printArray (char *arr, uint8_t len)
-{
-  
-  for (int i = 0; i <  len; i++)
-    { printf("%d ",arr[i]);}
-    printf("\n");
-    return;
-}
-
-
-
-int main()
-{
-    uint32_t max = 4294967295;
- 
-    uint32_t n = 3021966218;
-    
-//NumbStr to int implementation
-//Must be external func
- //   double sz = log10(n)+1;
- //   uint8_t sizen = sz;
-// 
-    
-    if (n > max){printf("ERROR: Invalid entry\n");return 0;}
-    if (n <= 0)  {printf("0.0.0.0\n");return 0;}
-    if (n < 256 && n > 0){printf("0.0.0.%u\n", n); return 0; }//8
- 
-if (n>255){
-     char *ext;
-     ext = (char*)malloc(36*sizeof(char));
-
-    inflow(&ext,n);
-    printArray(&ext, 36);
-    
-    free(ext); 
-}
- 
-    if (n < 4096){} //12
-    if (n < 65536){} //16
-    if (n < 1048576){} //20
-    if (n < 16777216){} //24
-    if (n < 268435456){} //28
-    if (n < 4294967296){} //32
- 
-    }
-
-
-###################
-// v2
-
-#include  <stdio.h>
-#include  <stdint.h>
-#include  <stdlib.h>
-
-static void printArray (char *arr, uint8_t len);//prints binArray
-
-char *arrBin(uint16_t size, uint32_t n)
+ char *arrBin(uint16_t size, uint32_t n)
 {
   if (size <= 0){return -1;}
   char *arr = malloc (sizeof(char) * size);
@@ -142,7 +53,7 @@ char *arrBin(uint16_t size, uint32_t n)
         
         if (inc == 8){ arr[inc] = '.'; inc++;}    
         if (inc == 17){arr[inc] = '.'; inc++;}
-        if (inc == 25){arr[inc] = '.'; inc++;}
+        if (inc == 26){arr[inc] = '.'; inc++;}
         
         if ( (n &(1<<i)) == 0) {arr[inc] = '0';inc++; }
         if ( (n &(1<<i)) != 0) {arr[inc] = '1';inc++; }
@@ -153,47 +64,26 @@ char *arrBin(uint16_t size, uint32_t n)
 
 
 
-
-int main()
+static void dPrint (char *arr, uint8_t st,  uint8_t nd )
 {
-    uint32_t n = 3021966218;
     
-   int *arr = arrBin(36, n);
-  printArray(arr, 36);
-    free(arr);
-}
-
-
-
-
-static void printArray (char *arr, uint8_t len)
-{
-  
-  for (int i = 0; i <  len; i++)
-    { printf("%c",arr[i]);}
-    printf("\n");
-    return;
-}
-
-
-//inside main
+ int8_t j = st-1;
+ uint8_t k = nd;
+ uint64_t inc = 1;
+   
  uint32_t tt = 0;
-    uint32_t inc = 1;
-    for (uint8_t i = 0; i < 4; i++){
-     for (uint8_t j = 27; j < 36; j++){
-        if (arr[26] == '1' && i == 0) {tt = 1;}
-        if (arr[18] == '1'&& i == 1) {tt = 1;}
-        if (arr[9] == '1'&& i == 2) {tt = 1;}
-        if (arr[0] == '1'&& i == 3) {tt = 1;}
-         
-         inc*=2;
-         if (arr[j] == '1'){tt+=inc;}
-         
+    if (arr[st] == '1') {tt = 1;}
+       
+     for ( ; j >= k; j--){ inc*=2;
+                           if (arr[j] == '1'){tt+=inc;}  }
         
-     }
+    printf ("%d" ,tt); 
+    st+=9;
+    nd+=9;
+    if (st <=34)
+    { printf ("."); return dPrint(arr,st,nd); }
         
-        inc = 1;
-        if(i == 1){j = 19}
-        if(i == 2){j = 10}
-    }
+    else {return;}
+
+}
 
